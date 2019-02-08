bf5936651499a6257dbae90dfb2473373c386b1f
JefDc <jefdc05@gmail.com>
2019-02-08 21:33:34 +0100

    add twig view contact on home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Contact;
+use App\Form\ContactType;
 use App\Repository\AboutUsRepository;
 use App\Repository\IntroRepository;
 use App\Repository\PortfolioRepository;
 use App\Repository\SkillRepository;
 use App\Repository\SoftSkillRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -17,14 +20,36 @@ class HomeController extends AbstractController
      */
     public function index(SkillRepository $skillRepository, IntroRepository $introRepository,
                           PortfolioRepository $portfolioRepository, AboutUsRepository $aboutUsRepository,
-                          SoftSkillRepository $softSkillRepository)
+                          SoftSkillRepository $softSkillRepository, Request $request)
     {
+        // Send message
+        $message = new Contact();
+        $form = $this->createForm(ContactType::class, $message);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($message);
+            $em->flush();
+
+            // Reinit form for redirection
+            $message = new Contact();
+            $form = $this->createForm( ContactType::class, $message);
+
+            $this->addFlash('success', 'Votre message a bien était envoyé. Je prendrai contact avec vous au plus tôt. Merci. ');
+
+            return $this->redirectToRoute('home', ['_fragment' => 'contact']);
+        }
+
+
+
         return $this->render('home/index.html.twig', [
             'skills' => $skillRepository->findAll(),
             'intros' => $introRepository->findAll(),
             'portfolios' => $portfolioRepository->findAll(),
             'intro' => $aboutUsRepository->findAll(),
-            'soft_skills' => $softSkillRepository->findAll()
+            'soft_skills' => $softSkillRepository->findAll(),
+            'form' => $form->createView()
         ]);
     }
 }
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,24 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('email')
-            ->add('message')
+            ->add('name', TextType::class, [
+                'attr' => [
+                    'class' => '',
+                    'placeholder' => 'Nom'
+                ]
+            ])
+            ->add('email', TextType::class, [
+                'attr' => [
+                    'class' => '',
+                    'placeholder' => 'Email'
+                ]
+            ])
+            ->add('message', TextareaType::class, [
+                'attr' => [
+                    'class' => '',
+                    'placeholder' => 'Message'
+                ]
+            ])
         ;
     }
 
