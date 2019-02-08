8d47bee693e4264a7104d54e967f0b2b2643cc59
JefDc <jefdc05@gmail.com>
2019-02-08 17:14:49 +0100

    add twig view skill

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SkillRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -10,10 +11,11 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(SkillRepository $skillRepository)
     {
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'skills' => $skillRepository->findAll()
         ]);
     }
 }
