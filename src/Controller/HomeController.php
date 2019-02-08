bdd6d69ea0892b9fd47573b916cfe3b9dbfe9ea1
JefDc <jefdc05@gmail.com>
2019-02-08 20:01:41 +0100

    add twig view soft skill on home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use App\Repository\AboutUsRepository;
 use App\Repository\IntroRepository;
 use App\Repository\PortfolioRepository;
 use App\Repository\SkillRepository;
+use App\Repository\SoftSkillRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,13 +16,15 @@ class HomeController extends AbstractController
      * @Route("/", name="home")
      */
     public function index(SkillRepository $skillRepository, IntroRepository $introRepository,
-                          PortfolioRepository $portfolioRepository, AboutUsRepository $aboutUsRepository)
+                          PortfolioRepository $portfolioRepository, AboutUsRepository $aboutUsRepository,
+                          SoftSkillRepository $softSkillRepository)
     {
         return $this->render('home/index.html.twig', [
             'skills' => $skillRepository->findAll(),
             'intros' => $introRepository->findAll(),
             'portfolios' => $portfolioRepository->findAll(),
-            'intro' => $aboutUsRepository->findAll()
+            'intro' => $aboutUsRepository->findAll(),
+            'soft_skills' => $softSkillRepository->findAll()
         ]);
     }
 }
