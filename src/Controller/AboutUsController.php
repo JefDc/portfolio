900174d103b686d5c577c11970e79dc7bf09925c
JefDc <jefdc05@gmail.com>
2019-02-28 10:18:24 +0100

    modify route admin/about

--- src/Controller/AboutUsController.php
+++ src/Controller/AboutUsController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/about")
+ * @Route("/admin/about")
  */
 class AboutUsController extends AbstractController
 {
