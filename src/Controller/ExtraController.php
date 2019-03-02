c2185fed6b4d7884f5e85717ba1db314dbbf8fce
JefDc <jefdc05@gmail.com>
2019-03-02 13:54:03 +0100

    add MailSettingType, add function mailSettingEdit

--- src/Controller/ExtraController.php
+++ src/Controller/ExtraController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\Extra;
+use App\Entity\MailSetting;
 use App\Entity\User;
 use App\Form\ExtraType;
+use App\Form\MailSettingType;
 use App\Form\UserType;
 use App\Repository\ExtraRepository;
+use App\Repository\MailSettingRepository;
 use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -40,7 +43,7 @@ class ExtraController extends AbstractController
             $this->getDoctrine()->getManager()->flush();
 
             return $this->redirectToRoute('extra_index', [
-                'id' => $extra->getId(),
+                'id' => $extra->getId()
             ]);
         }
 
@@ -50,4 +53,35 @@ class ExtraController extends AbstractController
         ]);
     }
 
+    /**
+     * @Route("/mail", name="extra_mail")
+     */
+    public function indexMail(MailSettingRepository $mailSettingRepository)
+    {
+        return $this->render('/admin/extra/mail/index.html.twig', [
+            'mailSettings' => $mailSettingRepository->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/mail/{id}/edit", name="extra_mail_edit", methods={"GET", "POST"})
+     */
+    public function mailEdit(Request $request, MailSetting $mailSetting)
+    {
+        $form = $this->createForm(MailSettingType::class, $mailSetting);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('extra_mail', [
+                'id' => $mailSetting->getId()
+            ]);
+        }
+
+        return $this->render('/admin/extra/mail/_form.html.twig', [
+           'mailSetting' => $mailSetting,
+           'form' => $form->createView()
+        ]);
+    }
 }
---
+++ src/Form/MailSettingType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\MailSetting;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class MailSettingType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('host', TextType::class, [
+                'attr' => [
+                    'class' => 'form-contol'
+                ]
+            ])
+            ->add('port', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                    ]
+            ])
+            ->add('encryption', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('username', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('password', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => MailSetting::class,
+        ]);
+    }
+}
